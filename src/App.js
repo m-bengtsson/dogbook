079a85a27b335c36df99f64a2da23ce5ba42c31a
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-03-02 14:20:26 +0100

    hardcoded homepage and some temporary styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import './App.css';
 
+function Header() {
+   return (
+      <div>
+         <header className='header'>
+            <h2>Dogbook</h2>
+         </header>
+      </div>
+   )
+}
+
+function Home() {
+   return (
+      <div>
+         <section className='main-content'>
+
+
+            <h1>Users</h1>
+            <ul>
+               <li>
+                  <a href='#'>@ Dog 1</a>
+               </li>
+               <li>
+                  <a href='#'>@ Dog 2</a>
+               </li>
+               <li>
+                  <a href='#'>@ Dog 3</a>
+               </li>
+            </ul>
+
+            <button>Create new dog</button>
+         </section>
+      </div>
+   )
+}
+
+
+
 function App() {
-    return (
-        <div>
+   return (
+      <div>
+         <Header />
+         <Home />
 
-
-        </div>
-    );
+      </div>
+   );
 }
 
 export default App;
